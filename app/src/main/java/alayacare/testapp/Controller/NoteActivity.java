a2f4175fc8197bd75d17f85a2d23b7e807c30b16
Paula Pithon <paulapithon@gmail.com>
2019-07-14 10:50:59 -0400

    TASK 2: display a note list on NoteActivity with mocked data

--- app/src/main/java/alayacare/testapp/Controller/NoteActivity.java
+++ app/src/main/java/alayacare/testapp/Controller/NoteActivity.java
@@ -1,15 +1,42 @@
 package alayacare.testapp.Controller;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import java.util.List;
+
+import alayacare.testapp.Model.NoteModel;
+import alayacare.testapp.Persistence.NoteViewModel;
 import alayacare.testapp.R;
 
 public class NoteActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
+    
+    private ListView noteListView;
+    private NoteItemAdapter noteListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        // Setup list view and its adapter
+        noteListView = findViewById(R.id.note_list);
+        noteListAdapter = new NoteItemAdapter(this, R.layout.note_list_item);
+        noteListView.setAdapter(noteListAdapter);
+
+        // Observe live data change and update adapter
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<NoteModel>>() {
+            @Override
+            public void onChanged(List<NoteModel> noteModels) {
+                noteListAdapter.setNotes(noteModels);
+            }
+        });
     }
 }
---
+++ app/src/main/java/alayacare/testapp/Controller/NoteItemAdapter.java
@@ -0,0 +1,73 @@
+package alayacare.testapp.Controller;
+
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.Date;
+import java.util.List;
+
+import alayacare.testapp.Model.NoteModel;
+import alayacare.testapp.R;
+
+public class NoteItemAdapter extends ArrayAdapter<NoteModel> {
+
+    private List<NoteModel> noteList;
+
+    public NoteItemAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.note_list_item, parent, false);
+            holder.noteText = convertView.findViewById(R.id.note_text);
+            holder.noteDate = convertView.findViewById(R.id.note_date);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        NoteModel note = noteList.get(position);
+        holder.noteText.setText(note.getText());
+
+        Date date = note.getDate();
+        DateFormat format = new DateFormat();
+        String formatDate = format.format("dd/MM/yy", date).toString();
+        holder.noteDate.setText(formatDate);
+
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        if (noteList != null)
+            return noteList.size();
+        else return 0;
+    }
+
+    /**
+     * Updates adapter note list to a new one
+     * @param noteList new note list
+     */
+    public void setNotes(List<NoteModel> noteList) {
+        this.noteList = noteList;
+        notifyDataSetChanged();
+    }
+
+    private class ViewHolder {
+        TextView noteText;
+        TextView noteDate;
+    }
+}
